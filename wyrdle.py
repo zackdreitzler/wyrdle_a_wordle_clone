19d22220b72e446d90f5723061576ce840717ee2
zackdreitzler <zdreitzler17@gmail.com>
2023-03-19 19:36:01 -0400

    step 1 completed

--- wyrdle.py
+++ wyrdle.py
@@ -1,18 +1,3 @@
-
-
-if __name__ == "__main__":
-    word_to_guess = get_guess_word()
-
-    for guess_num in range(1,7):
-        guess = input("Guess a five letter word: ").upper()
-        
-        if guess.upper() == "SNAKE":
-            print("RIGHT!")
-            break
-        else:
-            print("WRONG!")
-
-
 def get_guess_word():
     """
     Description: This is a function that grabs the word that will be used as the correct
@@ -21,3 +6,23 @@ def get_guess_word():
     Returns: A string representing the word to guess.
     """
     return "SNAKE"
+
+if __name__ == "__main__":
+    word_to_guess = get_guess_word()
+
+    for guess_num in range(1,7):
+        guess = input(f"\nGuess {guess_num}: ").upper()
+        
+        if guess == word_to_guess:
+            print("RIGHT!")
+            break
+        
+        correct_letters = {
+            letter for letter, correct in zip(guess, word_to_guess) if letter == correct
+        }
+        misplaced_letters = set(guess) & set(word_to_guess) - correct_letters
+        wrong_letters = set(guess) - set(word_to_guess)
+
+        print("Correct letters:", ", ".join(sorted(correct_letters)))
+        print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
+        print("Wrong letters:", ", ".join(sorted(wrong_letters)))
