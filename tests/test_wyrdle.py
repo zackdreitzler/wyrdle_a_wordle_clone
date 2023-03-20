c789bafdc52644a2647d4fe97e06f611b1fe4788
zackdreitzler <zdreitzler17@gmail.com>
2023-03-20 00:08:18 -0400

    created initial tests for check_guess function

--- tests/test_wyrdle.py
+++ tests/test_wyrdle.py
@@ -1,7 +1,33 @@
 import wyrdle
+from pytest import raises
 
 class TestWyrdle:
 
-    def test_get_guess_word(self):
-        word_list = ["SNAKE", "AORTA", "TIGER", "LEMON"]
-        assert wyrdle.get_guess_word() in word_list
+    def test_check_guess_not_winner(self):
+        """
+        Description:
+            This test checks to make sure the check_guess function correctly 
+            assigns a not winner condition with the correct values in the
+            positions list.
+        """
+        word_to_guess = "SNAKE"
+        guess = "SHIRK"
+
+        winner, positions = wyrdle.check_guess(guess, word_to_guess)
+        assert not winner 
+        assert positions == [1, 0, 0, 0, 2]
+
+
+    def test_check_guess_winner(self):
+        """
+        Description:
+            This test checks to make sure the check_guess function correctly 
+            assigns a winner condition with the correct values in the
+            positions list.
+        """
+        word_to_guess = "SNAKE"
+        guess = "SNAKE"
+
+        winner, positions = wyrdle.check_guess(guess, word_to_guess)
+        assert winner 
+        assert positions == [1, 1, 1, 1, 1]
