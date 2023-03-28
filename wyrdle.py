9abde54fc5f88415d56fb87ba4ceee28fc55d8bc
zackdreitzler <zdreitzler17@gmail.com>
2023-03-27 20:48:55 -0400

    added get_rich_formatted_str and set all guesses to uppercase

--- wyrdle.py
+++ wyrdle.py
@@ -6,21 +6,23 @@ from rich.console import Theme
 console = Console(width=100, theme=Theme({"warning": "red on yellow"}))
 
 
-def main():
+def main() -> None:
     """
     Description:
         This is a clone of the popular Wordle game. A player gets 6 guesses to guess
         a random 5 letter word. 
     """
     word_to_guess: str = get_guess_word()
-    
+    guesses_to_print: str = ""
     for guess_num in range(1,7):
         
         refresh_console(f"Guess: {guess_num}")
+        print_guesses(guesses_to_print)
 
         guess: str = get_guess()
-
         winner, positions = check_guess(guess, word_to_guess)
+
+        guesses_to_print += get_rich_formatted_str(positions, guess)
 
         if winner:
             print(f"Congrats! You Won!")
@@ -29,7 +31,7 @@ def main():
         print(f"The word was {word_to_guess}")
 
 
-def refresh_console(headline: str):
+def refresh_console(headline: str) -> None:
     """
     Description: 
         This refreshes the console and displays game information for the user.
@@ -38,6 +40,39 @@ def refresh_console(headline: str):
     """
     console.clear()
     console.rule(f"[bold blue]:computer: {headline} :computer:[/]\n")
+
+
+def print_guesses(guesses_to_print: str) -> None:
+    """
+    Description:
+        This prints the guesses to the display based on the following style.
+    Arguments:
+        guesses_to_print: str, a rich formatted string for printing each guess to the console.
+    """
+    console.print(guesses_to_print, justify="center")
+
+
+def get_rich_formatted_str(positions: list(int), guess: str) -> str:
+    """
+    Description:
+        This function takes the last guess and creates a rich styled string. This will be
+        used to print to the console.
+    Arguments:
+        postitions: list(int), a list of integers representing if the character is correct or not.
+        guess: str, the guess made by the user.
+    Returns:
+        result: str, a string formatted for printing to the console using rich.
+    """
+    result: str = ""
+    for p, g in positions, guess:
+        if p == 0:
+            result += (f"[white on #666666]{g}[/]")
+        elif p == 1:
+            result += (f"[bold white on green]{g}[/]")
+        else:
+            result += (f"[bold white on yellow]{g}[/]")
+    return result
+
 
 def check_guess(guess: str, word_to_guess: str) -> bool | list[int]:
     """
@@ -95,7 +130,7 @@ def get_guess() -> str:
         if len(guess) != 5:
             print("Please enter a guess of length 5.", flush=True)
         elif all([_.isalnum() for _ in guess]):
-            return guess
+            return guess.upper()
         else:
             print("Please enter all alphanumeric characters.", flush=True)
     
