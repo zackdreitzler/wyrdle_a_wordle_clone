1d5ad3823dbd13e20f2e61b7eb9645b4a0d0a61b
zackdreitzler <=>
2023-04-03 20:42:52 -0400

    Removed get word test file and removed pycache from git tracking

--- tests/test_get_word.py
+++
@@ -1,8 +0,0 @@
-import get_word
-
-class TestGetWord:
-
-    def test_get_guess_word(self):
-        word_list = ["SNAKE", "AORTA", "TIGER", "LEMON"]
-        assert get_word.get_guess_word() in word_list
-
